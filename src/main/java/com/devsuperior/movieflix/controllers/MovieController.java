758fe223309728274d3b1c4ce4c0c396a7d0ee77
lhp97 <lhdprado@gmail.com>
2023-02-14 08:23:42 -0300

    Movie repo and service

---
+++ src/main/java/com/devsuperior/movieflix/controllers/MovieController.java
@@ -0,0 +1,32 @@
+package com.devsuperior.movieflix.controllers;
+
+import com.devsuperior.movieflix.dto.MovieDTO;
+import com.devsuperior.movieflix.services.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieController {
+
+    @Autowired
+    private MovieService movieService;
+
+    @GetMapping
+    public ResponseEntity<List<MovieDTO>> findAll() {
+        List<MovieDTO> movieDTOS = movieService.findAll();
+        return ResponseEntity.ok().body(movieDTOS);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<MovieDTO> findById(@PathVariable Long id) {
+        MovieDTO movieDTO = movieService.findById(id);
+        return ResponseEntity.ok().body(movieDTO);
+    }
+}
---
+++ src/main/java/com/devsuperior/movieflix/services/MovieService.java
@@ -0,0 +1,32 @@
+package com.devsuperior.movieflix.services;
+
+import com.devsuperior.movieflix.dto.MovieDTO;
+import com.devsuperior.movieflix.entities.Movie;
+import com.devsuperior.movieflix.repositories.MovieRepository;
+import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class MovieService {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public List<MovieDTO> findAll() {
+        List<Movie> movies = movieRepository.findAll();
+        return movies.stream().map(x -> new MovieDTO(x)).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public MovieDTO findById(Long id) {
+        Optional<Movie> movieOptional = movieRepository.findById(id);
+        Movie movie = movieOptional.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+        return new MovieDTO(movie);
+    }
+}
