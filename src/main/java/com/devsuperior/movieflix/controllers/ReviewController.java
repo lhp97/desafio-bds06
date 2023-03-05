5220633264b4a8b65b568cdf743e9afcc2d21ace
lhp97 <lhdprado@gmail.com>
2023-03-05 15:24:53 -0300

    Ending Tests to ReviewResourceTI

--- src/main/java/com/devsuperior/movieflix/controllers/ReviewController.java
+++ src/main/java/com/devsuperior/movieflix/controllers/ReviewController.java
@@ -1,4 +1,4 @@
-package com.devsuperior.movieflix.controllers.exceptions;
+package com.devsuperior.movieflix.controllers;
 
 import com.devsuperior.movieflix.dto.ReviewDTO;
 import com.devsuperior.movieflix.services.ReviewService;
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
-@RequestMapping("/review")
+@RequestMapping("/reviews")
 public class ReviewController {
 
     @Autowired
@@ -21,5 +23,13 @@ public class ReviewController {
     public ResponseEntity<Page<ReviewDTO>> findAllPaged(Pageable pageable) {
         Page<ReviewDTO> reviewDTOS = reviewService.findAllPaged(pageable);
         return ResponseEntity.ok().body(reviewDTOS);
+    }
+
+    @PostMapping
+    public ResponseEntity<ReviewDTO> insertReview(@Valid @RequestBody ReviewDTO reviewDTO) {
+        ReviewDTO reviewInserted = reviewService.insert(reviewDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(reviewDTO.getId()).toUri();
+        return ResponseEntity.created(uri).body(reviewInserted);
+
     }
 }
--- src/main/java/com/devsuperior/movieflix/dto/ReviewDTO.java
+++ src/main/java/com/devsuperior/movieflix/dto/ReviewDTO.java
@@ -2,9 +2,12 @@ package com.devsuperior.movieflix.dto;
 
 import com.devsuperior.movieflix.entities.Review;
 
+import javax.validation.constraints.NotBlank;
+
 public class ReviewDTO {
 
     private Long id;
+    @NotBlank(message = "Campo requerido")
     private String text;
     private Long movieId;
     private UserDTO user;
